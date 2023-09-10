d1091f9cbc904494df6cbb3d142f13ce97787b08
66070252 <141353340+66070252@users.noreply.github.com>
2023-09-10 16:53:03 +0700

    Add files via upload

--- Project B.py
+++ Project B.py
@@ -1,32 +1,58 @@
 import pygame, sys
 
+#This create Player
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pygame.Surface((50,50))
+        self.image = pygame.Surface((30,30))
         self.image.fill((255,255,255))
         self.rect = self.image.get_rect(center = (screen_width/2,screen_height/2))
     
+    # Control follow cersor mouse
     def update(self):
         self.rect.center = pygame.mouse.get_pos()
 
+    def create_bullet(self):
+        return Bullet(pygame.mouse.get_pos()[0],pygame.mouse.get_pos()[1])
+
+#This create Bullet
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self,pos_x,pos_y):
+        super().__init__()
+        self.image = pygame.Surface((4,30))
+        self.image.fill((255,0,0))
+        self.rect = self.image.get_rect(center = (pos_x,pos_y))
+
+    def update(self):
+        self.rect.y -= 5
+
+#This section is Screen when run and add sumethings
 pygame.init()
 clock = pygame.time.Clock()
-screen_width, screen_height = 400,400
+screen_width, screen_height = 400,600
 screen = pygame.display.set_mode((screen_width,screen_height))
+pygame.mouse.set_visible(False)
 
 player = Player()
 player_group = pygame.sprite.Group()
 player_group.add(player)
 
+bullet_group = pygame.sprite.Group()
+
+#This section is run game
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-    
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            bullet_group.add(player.create_bullet())
+
+    #Drawing
     screen.fill((30,30,30))
     player_group.draw(screen)
+    bullet_group.draw(screen)
     player_group.update()
+    bullet_group.update()
     pygame.display.flip()
-    clock.tick(30)
+    clock.tick(120)
