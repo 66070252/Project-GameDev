763eba2f4ce8cf1158bad272abb9d9222046dfd2
66070252 <141353340+66070252@users.noreply.github.com>
2023-09-14 20:14:16 +0700

    Add files via upload

--- Project B.py
+++ Project B.py
@@ -1,58 +1,91 @@
-import pygame, sys
+#import function
+import pygame
+import os
+import time
+import random
+pygame.font.init()
 
-#This create Player
-class Player(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.image = pygame.Surface((30,30))
-        self.image.fill((255,255,255))
-        self.rect = self.image.get_rect(center = (screen_width/2,screen_height/2))
-    
-    # Control follow cersor mouse
-    def update(self):
-        self.rect.center = pygame.mouse.get_pos()
+#Screen
+width, height = 750, 750
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Space Ship War")
 
-    def create_bullet(self):
-        return Bullet(pygame.mouse.get_pos()[0],pygame.mouse.get_pos()[1])
+#Enemy images
+Red_ship = pygame.Surface((20,20))
+Red_ship.fill((255,0,0))
+Green_ship = pygame.Surface((20,20))
+Green_ship.fill((0,255,0))
+Blue_ship = pygame.Surface((20,20))
+Blue_ship.fill((0,0,255))
 
-#This create Bullet
-class Bullet(pygame.sprite.Sprite):
-    def __init__(self,pos_x,pos_y):
-        super().__init__()
-        self.image = pygame.Surface((4,30))
-        self.image.fill((255,0,0))
-        self.rect = self.image.get_rect(center = (pos_x,pos_y))
+#Player images
+Player_ship = pygame.Surface((20,20))
+Player_ship.fill((255,255,255))
 
-    def update(self):
-        self.rect.y -= 5
+#Bullet
+Bullet = pygame.Surface((3,20))
+Bullet.fill((255,0,0))
 
-#This section is Screen when run and add sumethings
-pygame.init()
-clock = pygame.time.Clock()
-screen_width, screen_height = 400,600
-screen = pygame.display.set_mode((screen_width,screen_height))
-pygame.mouse.set_visible(False)
+#Player character
+class Ship:
+    def __init__(self, x, y, health=100):
+        self.x = x
+        self.y = y
+        self.health = health
+        self.ship_img = None
+        self.laser_img = None
+        self.lasers = []
+        self.cool_down_counter = 0
 
-player = Player()
-player_group = pygame.sprite.Group()
-player_group.add(player)
+    def draw(self, window):
+        pygame.draw.rect(window, (255,0,0), (self.x, self.y, 50, 50))
 
-bullet_group = pygame.sprite.Group()
+#Background
+BG = pygame.transform.scale \
+    (pygame.image.load(os.path.join("BG.jpg")), (width,height))
 
-#This section is run game
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            bullet_group.add(player.create_bullet())
+#Game start
+def main():
+    run = True
+    FPS = 60
+    level = 1
+    lives = 5
+    main_font = pygame.font.SysFont("comicsans", 50)
+    clock = pygame.time.Clock()
+    ship = Ship(300, 650)
+    ship_speed = 5
 
-    #Drawing
-    screen.fill((30,30,30))
-    player_group.draw(screen)
-    bullet_group.draw(screen)
-    player_group.update()
-    bullet_group.update()
-    pygame.display.flip()
-    clock.tick(120)
+    #Screen
+    def draw():
+        screen.blit(BG, (0,0))
+        #text
+        live_label = main_font.render(f"lives: {lives}", 1, (255,255,255))
+        level_label = main_font.render(f"level: {level}", 1, (255,255,255))
+        #draw text
+        screen.blit(live_label, (10, 10))
+        screen.blit(level_label, (width - level_label.get_width() - 10, 10))
+
+        ship.draw(screen)
+
+        pygame.display.update()
+
+    #Make game run while loop
+    while run:
+        clock.tick(60)
+        draw()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        #Control
+        key = pygame.key.get_pressed()
+        if key[pygame.K_a] and ship.x - ship_speed > 0:
+            ship.x -= ship_speed
+        if key[pygame.K_w] and ship.y - ship_speed > 0:
+            ship.y -= ship_speed
+        if key[pygame.K_s] and ship.y + ship_speed + 50 < height:
+            ship.y += ship_speed
+        if key[pygame.K_d] and ship.x + ship_speed + 50 < width:
+            ship.x += ship_speed
+main()
