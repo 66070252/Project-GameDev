53b8882b1f3e2e53c1cee82d06db0606792db5c9
66070252 <141353340+66070252@users.noreply.github.com>
2023-09-16 23:17:26 +0700

    Add files via upload

--- Project B.py
+++ Project B.py
@@ -11,12 +11,9 @@ screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Space Ship War")
 
 #Enemy images
-Red_ship = pygame.Surface((20,20))
-Red_ship.fill((255,0,0))
-Green_ship = pygame.Surface((20,20))
-Green_ship.fill((0,255,0))
-Blue_ship = pygame.Surface((20,20))
-Blue_ship.fill((0,0,255))
+Red_ship = pygame.image.load(os.path.join("Images", "Red_Enemy.png"))
+Green_ship = pygame.image.load(os.path.join("Images", "Green_Enemy.png"))
+Blue_ship = pygame.image.load(os.path.join("Images", "Blue_Enemy.png"))
 
 #Player images
 Player_ship = pygame.image.load(os.path.join("Images", "player_ship.png"))
@@ -25,7 +22,7 @@ Player_ship = pygame.image.load(os.path.join("Images", "player_ship.png"))
 Bullet = pygame.Surface((3,20))
 Bullet.fill((255,0,0))
 
-#Player character
+#Create every character
 class Ship:
     def __init__(self, x, y, health=100):
         self.x = x
@@ -45,12 +42,30 @@ class Ship:
     def get_height(self):
         return self.ship_img.get_height()
 
+#Player character
 class Player(Ship):
     def __init__(self, x, y, health=100):
         super().__init__(x, y, health)
         self.ship_img = Player_ship
         self.mask = pygame.mask.from_surface(self.ship_img)
         self.max_health = health
+
+#Enemy Ship
+class Enemy(Ship):
+    COLOR_MAP = {
+                "red": (Red_ship),
+                "green": (Green_ship),
+                "blue": (Blue_ship)
+                }
+
+    def __init__(self, x, y, color, health=100):
+        super().__init__(x, y, health)
+        self.ship_img = self.COLOR_MAP[color]
+        self.mask = pygame.mask.from_surface(self.ship_img)
+    
+    #Move vertical line
+    def move(self, vel):
+        self.y += vel
 
 #Background
 BG = pygame.transform.scale \
@@ -61,32 +76,48 @@ def main():
     run = True
     FPS = 60
     level = 1
-    lives = 5
+    score = 0
     main_font = pygame.font.SysFont("comicsans", 50)
     clock = pygame.time.Clock()
     player = Player(300, 650)
     ship_speed = 5
+    enemies = []
+    wave_length = 5
+    enemy_val = 1
 
     #Screen
     def draw():
         screen.blit(BG, (0,0))
         
         #text
-        live_label = main_font.render(f"lives: {lives}", 1, (255,255,255))
+        score_label = main_font.render(f"score: {score}", 1, (255,255,255))
         level_label = main_font.render(f"level: {level}", 1, (255,255,255))
         
-        #draw text
-        screen.blit(live_label, (10, 10))
+        #Draw text
+        screen.blit(score_label, (10, 10))
         screen.blit(level_label, (width - level_label.get_width() - 10, 10))
 
+        #Draw Eney
+        for enemy in enemies:
+            enemy.draw(screen)
+
+        #Draw Player
         player.draw(screen)
 
         pygame.display.update()
 
     #Make game run while loop
     while run:
-        clock.tick(60)
-        draw()
+        clock.tick(FPS)
+
+        #Level of stage
+        if len(enemies) == 0:
+            score += 1
+            wave_length += 5
+            for i in range(wave_length):
+                enemy = Enemy(random.randrange(50, width-100), random.randrange(-1500, -100), \
+                              random.choice(["red", "green", "blue"]))
+                enemies.append(enemy)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -102,4 +133,11 @@ def main():
             player.y += ship_speed
         if key[pygame.K_d] and player.x + ship_speed + player.get_width() < width:
             player.x += ship_speed
+
+        #Enemy move
+        for enemy in enemies:
+            enemy.move(enemy_val)
+
+        draw()
+
 main()
