9d79a999439347dd1a005bd0c4edd620de9f1c54
66070251 <141380936+chaiviwat9023@users.noreply.github.com>
2023-10-05 21:58:24 +0700

    Update NewGameCode.py

--- NewGameCode.py
+++ NewGameCode.py
@@ -37,3 +37,84 @@ background_color2 = (0,0,255)
 gameDisplay = pygame.display.set_mode((display_width,display_height))
 pygame.display.set_caption('Project B')
 clock = pygame.time.Clock()
+
+def text_objects(text, font):
+    textSurface = font.render(text, True, blue)
+    return textSurface, textSurface.get_rect()
+
+def text_objects_blue(text, font):
+    textSurface = font.render(text, True, blue)
+    return textSurface, textSurface.get_rect()
+#Running game
+def game_intro():
+    game_loop()
+#Keep Game still running
+def game_loop():
+
+    choose_difficuly = True
+
+    left_clicked = True
+
+    #Choose difficuly
+    while choose_difficulty:
+      for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+          pygame.quit()
+          quit()
+
+      gameDisplay.fill(white)
+
+      largeText = pygame.font.Font('freesansbold.ttf', 50)
+      TextSurf, TextRect = text_objects("Difficulty", largeText)
+      TextRect.center = ((display_width/2),(display_height/7))
+      gameDisplay.blit(TextSurf, TextRect)
+
+      mouse_pos = pygame.mouse.get_pos()
+      x_mouse = mouse_pos[0]
+      y_mouse = mouse_pos[1]
+
+      mouse_pressed = pygame.mouse.get_pressed()
+
+      if x_mouse > 150 and x_mouse < 250 and y_mouse > 175 and y_mouse < 225:
+          pygame.draw.rect(gameDisplay, green,(150, 175, 100, 50))
+          if mouse_pressed[0] == 1 and left_clicked == False:
+              difficulty = 'easy'
+              choose_difficulty = False
+                
+      else:
+          pygame.draw.rect(gameDisplay, darkgreen,(150, 175, 100, 50))
+
+      if x_mouse > 350 and x_mouse < 450 and y_mouse > 175 and y_mouse < 225:
+          pygame.draw.rect(gameDisplay, green,(350, 175, 100, 50))
+          if mouse_pressed[0] == 1 and left_clicked == False:
+              difficulty = 'normal'
+              choose_difficulty = False
+                
+      else:
+          pygame.draw.rect(gameDisplay, darkgreen,(350, 175, 100, 50))
+
+      if x_mouse > 550 and x_mouse < 650 and y_mouse > 175 and y_mouse < 225:
+          pygame.draw.rect(gameDisplay, green,(550, 175, 100, 50))
+          if mouse_pressed[0] == 1 and left_clicked == False:
+              difficulty = 'hard'
+              choose_difficulty = False
+      else:
+          pygame.draw.rect(gameDisplay, darkgreen,(550, 175, 100, 50))
+
+      if x_mouse > 250 and x_mouse < 350 and y_mouse > 375 and y_mouse < 425:
+          pygame.draw.rect(gameDisplay, green,(250, 375, 100, 50))
+          if mouse_pressed[0] == 1 and left_clicked == False:
+              difficulty = 'very hard'
+              choose_difficulty = False
+                
+      else:
+          pygame.draw.rect(gameDisplay, darkgreen,(250, 375, 100, 50))
+
+      if x_mouse > 450 and x_mouse < 550 and y_mouse > 375 and y_mouse < 425:
+          pygame.draw.rect(gameDisplay, green,(450, 375, 100, 50))
+          if mouse_pressed[0] == 1 and left_clicked == False:
+              difficulty = 'extreme'
+              choose_difficulty = False
+
+      else:
+          pygame.draw.rect(gameDisplay, darkgreen,(450, 375, 100, 50))
